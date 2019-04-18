71c168b70d46ac9c64a1d78784b1d4615260a482
Gleb Zverinskiy <gleb_zverinskiy@symantec.com>
2019-04-18 08:35:16 -0400

    Adding encryption functionality to S3 lambda.
    * Change permissions in CFT template
    * Add check id to list of handled events
    * Refactor s3 lambda to handle multiple functions as a map of check id
    * Implement s3 encryption to AES-256

--- AWS remediation scripts/SYMC_CWA_Remediation_Worker_S3/lambda_function.py
+++ AWS remediation scripts/SYMC_CWA_Remediation_Worker_S3/lambda_function.py
@@ -1,96 +1,89 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import json
 import boto3
 import sys
+import time
 from raise_sns_notification import *
 
-def lambda_handler(event, context):
-    remediation_check_map = dict()
-    cloud_provider_config = {}
-    try:
-        print('symc_cwa_remediation_worker_s3_called')
-        print(event)
-        print('Remediating '+event['resource']['id']+' in region '+event['resource']['region'])
+def encrypt_bucket(s3bucket):
+    client = boto3.client('s3')
+    print ('Enable Bucket encryption for - ' + s3bucket)
+    response = client.put_bucket_encryption(Bucket=s3bucket, ServerSideEncryptionConfiguration={'Rules':[
+        {'ApplyServerSideEncryptionByDefault': {'SSEAlgorithm': 'AES256'}}]})
+    print ('response from bucket encryption  - ' + str(response))
+    # check if bucket is encrypted
+    bucket_encrypt_status = get_bucket_encryption(client, s3bucket)
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200 and bucket_encrypt_status:
+        return True
+    else:
+        return False
 
-        generateAndSendRemediationOutput = False
-        for eventKeys in event:
-            if(eventKeys == "cloud_provider_config"):
-                generateAndSendRemediationOutput = True
-                cloud_provider_config = event['cloud_provider_config']
-                print (eventKeys)
-                break
-
-        if(generateAndSendRemediationOutput):
-            print('Remediation Cloud Provider Configuration : ' + json.dumps(cloud_provider_config))
-        else:
-            print('Remediation Cloud Provider Configuration Not Provided')
-
-
-        remediation_output_message = {}
-        if(generateAndSendRemediationOutput):
-            remediation_output_message["payload_id"] = event['payload_id']
-            remediation_output_message["remediated_checks"] = []
-
-        s3VersionEnabled = False
-
-        s3VersionenabledCheck='282ac909-af9c-4c8e-81d7-e1c23533378b'
-
-        for check in event['checks']:
-            if(check['id'].lower()== s3VersionenabledCheck):
-                if(generateAndSendRemediationOutput):
-                    remediation_check_obj = {}
-                    remediation_check_map[s3VersionenabledCheck]=remediation_check_obj
-                    remediation_check_obj['id']=check['id']
-                    remediation_check_obj['status']=0
-                    remediation_check_obj['version']=check['version']
-                s3VersionEnabled = True
-
-
+def get_bucket_encryption(client, s3bucket):
+    result = None
+    for i in range(0,5):
+        time.sleep(i)
         try:
-            if(s3VersionEnabled):
-                print('Get S3 Bucket')
-                S3Bucket = str(event['resource']['id'])
-                print('Get S3 Resource of the bucket')
-                s3 = boto3.resource('s3')
-                print('Enable Bucket versioning for - ' + S3Bucket)
-                bucket_versioning = s3.BucketVersioning(S3Bucket)
-                response = bucket_versioning.enable();
-                print("response from bucket versionsing  - " + str(response))
-
-
-            if(generateAndSendRemediationOutput):
-                for k in remediation_check_map:
-                    if(k.lower() == s3VersionenabledCheck):
-                        remediation_check_obj = remediation_check_map[s3VersionenabledCheck]
-                        remediation_check_obj['status']=0
-                        remediation_check_obj['message']='Remediated Succesfully from AWS Lambda'
-                        remediation_output_message["remediated_checks"].append(remediation_check_obj)
-
-            print('Remediation successful.')
-
+            result = client.get_bucket_encryption(Bucket=s3bucket)
         except:
-            type, value, traceback = sys.exc_info()
-            print(value)
-            print('Error occurred while calling S3 API ' + str(sys.exc_info()[0]))
-            errorMessage = str(sys.exc_info()[0])
+            pass
+        if result:
+            print("Bucket encryption policy after encryption: {}"
+                  .format(result['ServerSideEncryptionConfiguration']['Rules']))
+            break
+    return result
 
-            for k in remediation_check_map:
-                if(k.lower() == s3VersionenabledCheck):
-                    remediation_check_obj = remediation_check_map[s3VersionenabledCheck]
-                    remediation_check_obj['status']=1
-                    remediation_check_obj['message']=errorMessage
-                    remediation_output_message["remediated_checks"].append(remediation_check_obj)
+def enable_version_on_bucket(s3bucket):
+    s3 = boto3.resource('s3')
+    print('Enable Bucket versioning for - ' + s3bucket)
+    bucket_versioning = s3.BucketVersioning(s3bucket)
+    response = bucket_versioning.enable()
+    print ('response from bucket versioning  - ' + str(response))
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return True
+    else:
+        return False
 
-        if(generateAndSendRemediationOutput):
-               remediation_output_message_str = json.dumps(remediation_output_message)
-               print('Remediation Output Message Generated : ' + remediation_output_message_str)
-               remediation_output_message_str_json = prepareSNSJsonMessage(remediation_output_message_str)
-               raise_sns_notification(cloud_provider_config['output_sns_topic_arn'],cloud_provider_config['output_sns_topic_region'],remediation_output_message_str_json,'json')
-               print('Remediation Output Message Published Succesfully to SNS.')
+    
+def return_bucket(event):
+    try:
+        return str(event['resource']['id'])
+    except KeyError:
+        return None
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Remediation Performed Succesfully')
-        }
-    except:
-        print ("Unexpected error:", str(sys.exc_info()[0]))
-        raise
+def lambda_handler(event, context):
+    remediation_status = None
+    # map of functions and check ids
+    check_id_to_func = {'c2ec4814-76c4-4413-840f-b2b0766d0077': encrypt_bucket,
+                        '282ac909-af9c-4c8e-81d7-e1c23533378b': enable_version_on_bucket}
+    remediation_output = {"payload_id": None, 
+                          "remediated_checks": [{"id": None, "status": 0, "version": "1.0.0", 
+                                                 "message": "Remediated Succesfully from AWS Lambda"}]}
+    
+    print("event: {}".format(event))
+    
+    bucket_to_act_on = return_bucket(event)
+    check_id =  event['checks'][0]['id']
+    print ('symc_cwa_remediation_worker_s3_called')
+    print ('Remediating ' + event['resource']['id'] + ' in region ' + event['resource']['region'])
+    
+    try:
+        # perform the remediation
+        remediation_status = check_id_to_func[check_id](bucket_to_act_on)
+    except Exception as e:
+        print ('Something went wrong: {}'.format(e))
+     
+    if remediation_status:
+        #  populate data into output
+        remediation_output['payload_id'] = event['payload_id']
+        remediation_output['remediated_checks'][0]['id'] = check_id
+        print("Remediated Output Message Generated: {}".format(remediation_output))
+        
+        #  only perform if output sns topic is provided
+        if event.get('cloud_provider_config'):
+            sns_message = prepareSNSJsonMessage(json.dumps(remediation_output))
+            raise_sns_notification(event['cloud_provider_config']['output_sns_topic_arn'],
+                                   event['cloud_provider_config']['output_sns_topic_region'],
+                                   sns_message, 'json')
+            print ('Remediation Output Message Published Succesfully to SNS.')
+        return {'statusCode': 200, 'body': 'Remediation Performed Succesfully'}
